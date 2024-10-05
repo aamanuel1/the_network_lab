1c73e79f6eff2df4582858d08387f1f2c91fce75
Aaron Manuel <aaron.manuel@ucalgary.ca>
2024-10-05 11:29:50 -0600

    Outline implementation for project 2

--- a_better_web_server_project2/webserver.py
+++ a_better_web_server_project2/webserver.py
@@ -15,15 +15,24 @@ new_conn = s.accept()
 new_socket = new_conn[0]
 print(new_conn)
 
-response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n"\
-            "Content-Length: 6\r\n"\
-            "Connection: close\r\n\r\nHello!"
-encode_response = response.encode("ISO-8859-1")
 
 while True:
+    #Parse request header
     d = new_socket.recv(4096)
     print(d)
     eof = re.search("\r\n\r\n", d.decode("ISO-8859-1"))
+
+    #Strip the path
+
+    #Read and determine data type
+
+    #Build the response
+    response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n"\
+            "Content-Length: 6\r\n"\
+            "Connection: close\r\n\r\nHello!"
+    encode_response = response.encode("ISO-8859-1")
+
+    #Send response
     if eof is not None:
         new_socket.sendall(encode_response)
         new_socket.close()
