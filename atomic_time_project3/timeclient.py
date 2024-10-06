fc78e7afbb8ed05d145dea576e241b381a30cd03
Aaron Manuel <aaron.manuel@ucalgary.ca>
2024-10-06 11:08:37 -0600

    Finished timeclient.py

--- atomic_time_project3/timeclient.py
+++ atomic_time_project3/timeclient.py
@@ -1,6 +1,7 @@
 #timeclient.py - Queries and compares atomic time from time.nist.gov
 #Part of Beej's Guide to Network Concepts, project 3.
 import time
+import socket
 
 def system_seconds_since_1900():
     """
@@ -17,15 +18,28 @@ def system_seconds_since_1900():
 
 def main():
     #Connect to the server
+    #Form request
+    target = "time.nist.gov"
+    port = 37
+    request = "GET / HTTP/1.1\r\nHost: " + target + "\r\nConnection: close\r\n\r\n"
+    encoded_request = request.encode("ISO-8859-1")
 
+    s = socket.socket()
+    s.connect((target, port))
+    s.sendall(encoded_request)
+    
     #Receive data
+    nist_time_bytes = s.recv(4096)
 
     #Decode data
+    nist_time = int.from_bytes(nist_time_bytes, "big")
 
     #Print out NIST's time
+    print("NIST time\t: {}".format(nist_time))
 
     #Print out system time
-    pass
+    system_time = system_seconds_since_1900()
+    print("System time\t: {}".format(system_time))
 
 if __name__ == '__main__':
     main()
