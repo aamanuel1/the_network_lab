ae6a2575f0f9bae6bac58968c264a299f04a4fd4
Aaron <aarmanuel@gmail.com>
2025-12-17 21:19:50 -0700

    Prototype for ip to bytes function seems like it works.

--- validate_tcp_packets_project5/validate_tcp.py
+++ validate_tcp_packets_project5/validate_tcp.py
@@ -1,6 +1,26 @@
 import sys
-def main(argv):
-    pass
+
+def main():
+    filename = "./tcp_data/tcp_addrs_0.txt"
+
+    with open(filename, 'r') as file:
+        ip_addr = file.readline().split()
+
+    ip_addr_bytes = []
+    for ip in ip_addr:
+        print(ip)
+        subnet_bytes = ip.split(".")
+        ip_bytestring = bytearray()
+        for index, ip_byte in enumerate(subnet_bytes):
+            ip_byte = int(ip_byte)
+            ip_byte = ip_byte.to_bytes(1, "big")
+            ip_bytestring += ip_byte
+        # ip_addr_bytes.append(subnet_bytes)
+        print(ip_bytestring.hex(" "))
+        ip_addr_bytes
+        print(ip_addr_bytes)
+        
+        
 
 if __name__ == "__main__":
     main()
