b08854829fb78ccdc4a7187a2bd694a280d898d9
Aaron <aarmanuel@gmail.com>
2025-12-22 15:03:18 -0700

    add some comments

--- udp_example/udpclient.py
+++ udp_example/udpclient.py
@@ -10,11 +10,14 @@ except:
     print("usage: udpclient.py server port message", file=sys.stderr)
     sys.exit(1)
 
+#Make UDP socket
 s = socket.socket(type=socket.SOCK_DGRAM)
 
+#Send to the server and port in args
 print("Sending message...")
 s.sendto(message.encode(), (server, port))
 
+#Get the reply
 data, sender = s.recvfrom(4096)
 print(f"Got reply: \"{data.decode()}\"")
 
