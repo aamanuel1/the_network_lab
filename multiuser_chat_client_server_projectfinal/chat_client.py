5fee3020d1b2c828179b14be06aa3e0dbad6f539
Aaron <aarmanuel@gmail.com>
2026-02-04 21:34:26 -0700

    skeleton functions to give a starting idea.

---
+++ multiuser_chat_client_server_projectfinal/chat_client.py
@@ -0,0 +1,40 @@
+#Final project
+#chat_client.py
+#Example usage: python chat_client somedude localhost 42069
+
+import sys
+import socket
+from chatui import init_windows, read_command, print_message, end_windows
+
+def usage():
+    print("usage: chat_client.py nick host port", file = sys.stderr)
+
+def get_next_packet():
+    pass
+
+def input():
+    pass
+
+def create_hello_message():
+    pass
+
+def create_chat_message():
+    pass
+
+def extract_message():
+    pass
+
+def parse_message():
+    pass
+
+def broadcast_message():
+    pass
+
+def close_conn():
+    pass
+
+def main(argv):
+    pass
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
