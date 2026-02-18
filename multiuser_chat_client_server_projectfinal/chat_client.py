de4ec8f8a7dd8edaccf4675f6587d0efebd78056
Aaron <aarmanuel@gmail.com>
2026-02-18 00:12:50 -0700

    Completed message formation and main input loop

--- multiuser_chat_client_server_projectfinal/chat_client.py
+++ multiuser_chat_client_server_projectfinal/chat_client.py
@@ -5,22 +5,45 @@
 import sys
 import socket
 from chatui import init_windows, read_command, print_message, end_windows
+import json
+
+PKT_LEN_SIZE = 2
 
 def usage():
     print("usage: chat_client.py nick host port", file = sys.stderr)
 
-def get_next_packet():
-    pass
+def get_next_packet(socket, buffer):
+    data = socket.recv(4096)
+    buffer[socket] += data
 
 def chat_window():
     #loop - get packets, extract and parse full message, then print message
     pass
 
-def create_hello_message():
-    pass
+def create_hello_message(nick):
+    hello_message_dict = {
+        "type": "hello",
+        "nick": f"{nick}"
+    }
+    hello_pkt = create_pkt(hello_message_dict)
+    return hello_pkt
 
-def create_chat_message():
-    pass
+def create_chat_message(message):
+    chat_message_dict = {
+        "type": "chat",
+        "message": f"{message}"
+    }
+    chat_pkt = create_pkt(chat_message_dict)
+    return chat_pkt
+
+def create_pkt(message_dict):
+    message_json = json.dumps(message_dict)
+    print_message(message_json)
+    message_size = len(message_json)
+    message_bytes = message_json.encode()
+    message_size_bytes = message_size.to_bytes(PKT_LEN_SIZE, "big")
+    message_pkt = message_size_bytes + message_bytes
+    return message_pkt
 
 def extract_message():
     pass
@@ -28,11 +51,16 @@ def extract_message():
 def parse_message():
     pass
 
+def parse_special_input(command, sock):
+    if command.strip() == "\\q":
+        close_conn(sock)
+        exit(0)
+
 def broadcast_message():
     pass
 
-def close_conn():
-    pass
+def close_conn(sock):
+    sock.close()
 
 def main(argv):
     try:
@@ -44,15 +72,34 @@ def main(argv):
         return 1
     
     #Connect to socket
+    s = socket.socket()
+    s.connect((host, port))
 
 
     #Start second thread with looping input()
 
 
     #Create and send hello message
-
+    hello_message_pkt = create_hello_message(nick)
+    s.sendall(hello_message_pkt)
+    # print_message(str(hello_message_pkt))
     
     #Main input loop - get message from user, package it into packets and send it 
+    while True:
+        try:
+            command = read_command(f"{nick}> ")
+        except:
+            break
+
+        if command == "":
+            continue
+
+        if command[0] == "\\":
+            parse_special_input(command, s)
+
+        chat_message_pkt = create_chat_message(command)
+        s.sendall(chat_message_pkt)
+        # print_message(str(chat_message_pkt))
 
 
 
