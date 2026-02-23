a86ab1ee1547918154cfdb14872c714f49beeca4
Aaron <aarmanuel@gmail.com>
2026-02-23 01:31:06 -0700

    add chat window and other things

--- multiuser_chat_client_server_projectfinal/chat_client.py
+++ multiuser_chat_client_server_projectfinal/chat_client.py
@@ -6,6 +6,7 @@ import sys
 import socket
 from chatui import init_windows, read_command, print_message, end_windows
 import json
+import threading
 
 PKT_LEN_SIZE = 2
 
@@ -14,11 +15,28 @@ def usage():
 
 def get_next_packet(socket, buffer):
     data = socket.recv(4096)
-    buffer[socket] += data
+    buffer += data
 
-def chat_window():
+def chat_window(socket, buffer):
     #loop - get packets, extract and parse full message, then print message
-    pass
+    pkt_size = None
+    pkt_total_length = None
+    while True:
+        get_next_packet(socket, buffer)
+        if pkt_size is None:
+            pkt_size = int.from_bytes(buffer[:PKT_LEN_SIZE], "big")
+            pkt_total_length = pkt_size + PKT_LEN_SIZE
+
+        if len(buffer) >= pkt_total_length:
+            message = extract_message(buffer, pkt_total_length)
+
+            if message is None:
+                continue
+
+            parse_incoming_message(message)
+            pkt_size = None
+            pkt_total_length = None
+
 
 def create_hello_message(nick):
     hello_message_dict = {
@@ -40,16 +58,38 @@ def create_pkt(message_dict):
     message_json = json.dumps(message_dict)
     print_message(message_json)
     message_size = len(message_json)
+    print_message(str(message_size))
     message_bytes = message_json.encode()
     message_size_bytes = message_size.to_bytes(PKT_LEN_SIZE, "big")
+    message_pkt = bytearray()
+    message_pkt.extend(message_size_bytes)
+    message_pkt.extend(message_bytes)
     message_pkt = message_size_bytes + message_bytes
     return message_pkt
 
-def extract_message():
-    pass
+def extract_message(buffer, msg_length):
+    if len(buffer) == 0:
+        return None
+    
+    msg_bytes = buffer[PKT_LEN_SIZE:PKT_LEN_SIZE + msg_length]
+    msg = msg_bytes.decode()
 
-def parse_message():
-    pass
+    return msg
+
+def parse_incoming_message(message):
+    message_json = json.loads(message)
+    message_type = message_json["type"]
+    sender_nick = message_json["nick"]
+
+    match message_type:
+        case "chat":
+            chat_message = message_json["chat"]
+            print_message(f"{sender_nick}: {chat_message}")
+        case "join":
+            print_message(f"*** {sender_nick} has joined the chat")
+        case "leave":
+            print_message(f"*** {sender_nick} has left the chat")
+
 
 def parse_special_input(command, sock):
     if command.strip() == "\\q":
@@ -75,9 +115,13 @@ def main(argv):
     s = socket.socket()
     s.connect((host, port))
 
+    buffer = b''
 
+    init_windows()
     #Start second thread with looping input()
-
+    #Thread args has to be in a tuple for some reason.
+    input_thread = threading.Thread(target=chat_window, args=(s, buffer), daemon=True)
+    input_thread.start()
 
     #Create and send hello message
     hello_message_pkt = create_hello_message(nick)
@@ -100,6 +144,8 @@ def main(argv):
         chat_message_pkt = create_chat_message(command)
         s.sendall(chat_message_pkt)
         # print_message(str(chat_message_pkt))
+    
+    end_windows()
 
 
 
